a6e50495cd0285fb7661854378a0fa820d437557
Yuhan Long <yuhan@yuhans-mbp.wv.cc.cmu.edu>
2014-10-04 15:08:21 -0400

    modified framework

--- task6/task6.ino
+++ task6/task6.ino
@@ -3,5 +3,19 @@
 //Created by Yuhan Long
 //this work belongs to evolutus
 //all right reserved 
+void dsetup(){
+  
+ //for setup code of pins and interrupts.
+}
 
+void timerInterrup(){
+  //read all sensor,
+  //send new comments to motors, 
+  
+}
 
+void loop(){
+  //read the serial input and report data to the server.
+   
+}
+  
